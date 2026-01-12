a5b527e12e7b6ee6d859d315166b7cf61d4e0660
nesple69 <nesple@gmail.com>
2026-01-12 20:45:44 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -410,7 +410,7 @@ async function checkAvailability() {
   }
 
   // 4. Calendar Check (Apps Script)
-  const scriptUrl = 'https://script.google.com/macros/s/AKfycbyasci2vpHy810wD7x4-Vzjn2EgKFexggA_-Xcwp0E-cprNq6ttiZj-1MTICscpAdtN/exec';
+  const scriptUrl = 'https://script.google.com/macros/s/AKfycbyc4Cp5ijb_Ya72Rj4S4sRMQWrdtrimiBwhGUd9ISZAjYg3IFCoeuFrdAhv1ywO4zBt/exec';
   try {
     const response = await fetch(`${scriptUrl}?date=${dateStr}&time=${time}`);
     const result = await response.json();
@@ -589,7 +589,7 @@ document.addEventListener('DOMContentLoaded', () => {
       totale: document.getElementById('totalSpan').textContent
     };
 
-    const scriptUrl = 'https://script.google.com/macros/s/AKfycbyasci2vpHy810wD7x4-Vzjn2EgKFexggA_-Xcwp0E-cprNq6ttiZj-1MTICscpAdtN/exec';
+    const scriptUrl = 'https://script.google.com/macros/s/AKfycbyc4Cp5ijb_Ya72Rj4S4sRMQWrdtrimiBwhGUd9ISZAjYg3IFCoeuFrdAhv1ywO4zBt/exec';
 
     try {
       // Usiamo 'text/plain' per evitare il preflight CORS che Apps Script non gestisce bene
