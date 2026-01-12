a4a975b27294099f21884026e65f55f0949fc6c5
nesple69 <nesple@gmail.com>
2026-01-12 11:53:25 +0100

    aggiornamento colonne e prenotazioni

--- script.js
+++ script.js
@@ -410,7 +410,7 @@ async function checkAvailability() {
   }
 
   // 4. Calendar Check (Apps Script)
-  const scriptUrl = 'https://script.google.com/macros/s/AKfycbydvI8J9oc0CMWk16fcv9ohRZPbpfZM0Cuq0h4DYXW1KiOVgQGvOEAx-tBYB3EGxF4x/exec';
+  const scriptUrl = 'https://script.google.com/macros/s/AKfycbxcxlmSzNr0wYHZ5HfGaJUFbV5zy3FBhW4vcsPlZs3Qoy1UDO0KKMFk66aLd2cXfTZJ/exec';
   try {
     const response = await fetch(`${scriptUrl}?date=${dateStr}&time=${time}`);
     const result = await response.json();
@@ -589,7 +589,7 @@ document.addEventListener('DOMContentLoaded', () => {
       totale: document.getElementById('totalSpan').textContent
     };
 
-    const scriptUrl = 'https://script.google.com/macros/s/AKfycbydvI8J9oc0CMWk16fcv9ohRZPbpfZM0Cuq0h4DYXW1KiOVgQGvOEAx-tBYB3EGxF4x/exec';
+    const scriptUrl = 'https://script.google.com/macros/s/AKfycbxcxlmSzNr0wYHZ5HfGaJUFbV5zy3FBhW4vcsPlZs3Qoy1UDO0KKMFk66aLd2cXfTZJ/exec';
 
     try {
       // Usiamo 'text/plain' per evitare il preflight CORS che Apps Script non gestisce bene
