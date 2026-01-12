7b9d20551f219043fecf794650d1f70028401e2e
nesple69 <nesple@gmail.com>
2026-01-12 14:27:38 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -410,7 +410,7 @@ async function checkAvailability() {
   }
 
   // 4. Calendar Check (Apps Script)
-  const scriptUrl = 'https://script.google.com/macros/s/AKfycbwHNlRGahJTwn4IG18JDqcwIB4Xu44neIXwGj5vOHy4hYIb7DcOWCgJNei06AGobosc/exec';
+  const scriptUrl = 'https://script.google.com/macros/s/AKfycbw4fun7MnBU5hNm__o9HgsUMVhzY_7UCwfGSd2fLqR__cbm8efZEMmNqMAs5FGzNSFt/exec';
   try {
     const response = await fetch(`${scriptUrl}?date=${dateStr}&time=${time}`);
     const result = await response.json();
@@ -589,7 +589,7 @@ document.addEventListener('DOMContentLoaded', () => {
       totale: document.getElementById('totalSpan').textContent
     };
 
-    const scriptUrl = 'https://script.google.com/macros/s/AKfycbwHNlRGahJTwn4IG18JDqcwIB4Xu44neIXwGj5vOHy4hYIb7DcOWCgJNei06AGobosc/exec';
+    const scriptUrl = 'https://script.google.com/macros/s/AKfycbw4fun7MnBU5hNm__o9HgsUMVhzY_7UCwfGSd2fLqR__cbm8efZEMmNqMAs5FGzNSFt/exec';
 
     try {
       // Usiamo 'text/plain' per evitare il preflight CORS che Apps Script non gestisce bene
